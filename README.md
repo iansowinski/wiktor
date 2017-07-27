a0cc37bd1053dcff0ff0882cd26126a1ac637598
Ian Sowinski <sowinj@gmail.com>
2017-07-27 21:54:33 +0200

    first things first - learning how pir works

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ## Wymagania
 
-
-Działa tylko na mac os sierra z zainstalowanym pythonem 2 i pythonem 3. wymaga [imagesnap](http://iharder.sourceforge.net/current/macosx/imagesnap/).
+Działa tylko na mac os sierra z zainstalowanym pythonem 2 i pythonem 3. Wymaga [imagesnap](http://iharder.sourceforge.net/current/macosx/imagesnap/).
 
 [Sterowniki](https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver)
+
 [Arduino IDE](https://www.arduino.cc/en/Guide/ArduinoNano)
 
 pliki odpalamy w tej kolejności:
--- arduino_controller.c
+++ arduino_controller.c
@@ -35,37 +35,39 @@ void setup(){
 //LOOP
 void loop(){
   if(digitalRead(pirPin) == HIGH){
-    Serial.print("HIGH");
-    digitalWrite(LED_BUILTIN, LOW);   //the led visualizes the sensors output pin state
-    if(lockLow){ 
-      //makes sure we wait for a transition to LOW before any further output is made:
-      lockLow = false;           
-      Serial.println("---");
-      Serial.print("motion detected at ");
-      Serial.print(millis()/1000);
-      Serial.println(" sec");
-      delay(50);
-    }        
-    takeLowTime = true;
-    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("high");
+    delay(1000);
+    // digitalWrite(LED_BUILTIN, LOW);   //the led visualizes the sensors output pin state
+    // if(lockLow){ 
+    //   //makes sure we wait for a transition to LOW before any further output is made:
+    //   lockLow = false;           
+    //   Serial.println("---");
+    //   Serial.print("motion detected at ");
+    //   Serial.print(millis()/1000);
+    //   Serial.println(" sec");
+    //   delay(50);
+    // }        
+    // takeLowTime = true;
+    // digitalWrite(LED_BUILTIN, HIGH);
   }
   if(digitalRead(pirPin) == LOW){
-    Serial.print("LOW");
-    digitalWrite(LED_BUILTIN, HIGH);  //the led visualizes the sensors output pin state
-    if(takeLowTime){
-      lowIn = millis();          //save the time of the transition from high to LOW
-      takeLowTime = false;       //make sure this is only done at the start of a LOW phase
-    }
-    //if the sensor is low for more than the given pause,
-    //we assume that no more motion is going to happen
-    if(!lockLow && millis() - lowIn > pause){ 
-      //makes sure this block of code is only executed again after
-      //a new motion sequence has been detected
-      lockLow = true;                       
-      Serial.print("motion ended at ");      //output
-      Serial.print((millis() - pause)/1000);
-      Serial.println(" sec");
-      delay(50);
-    }
+    Serial.println("low");
+    delay(1000);
+    // digitalWrite(LED_BUILTIN, HIGH);  //the led visualizes the sensors output pin state
+    // if(takeLowTime){
+    //   lowIn = millis();          //save the time of the transition from high to LOW
+    //   takeLowTime = false;       //make sure this is only done at the start of a LOW phase
+    // }
+    // //if the sensor is low for more than the given pause,
+    // //we assume that no more motion is going to happen
+    // if(!lockLow && millis() - lowIn > pause){ 
+    //   //makes sure this block of code is only executed again after
+    //   //a new motion sequence has been detected
+    //   lockLow = true;                       
+    //   Serial.print("motion ended at ");      //output
+    //   Serial.print((millis() - pause)/1000);
+    //   Serial.println(" sec");
+    //   delay(50);
+    // }
   }
 }
