efaa39b01902ee0545a55815806ff64535ffc8d3
Ian Sowinski <sowinj@gmail.com>
2017-09-27 11:38:53 +0200

    go fmt

--- go/main.go
+++ go/main.go
@@ -1,51 +1,50 @@
 package main
 
 import (
-  "fmt"
-  "time"
-  "bufio"
-  "os"
-  "os/exec"
-  "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
-
-func main(){
-  whereIAm()
-  commandCatcher()
+func main() {
+	whereIAm()
+	commandCatcher()
 }
 
 func commandCatcher() {
-  reader := bufio.NewReader(os.Stdin)
-  for true {
-    fmt.Print("command: ")
-    text, _ := reader.ReadString('\n')
-    if text == "snap\n" {
-      go snap()
-    }
-    if text == "exit\n" {
-      break
-    }
-  }
+	reader := bufio.NewReader(os.Stdin)
+	for true {
+		fmt.Print("command: ")
+		text, _ := reader.ReadString('\n')
+		if text == "snap\n" {
+			go snap()
+		}
+		if text == "exit\n" {
+			break
+		}
+	}
 }
 
-func whereIAm(){
-  out, err := exec.Command("pwd").Output()
-  if err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
-  fmt.Printf("your output will be stored in: %s\n", out)
+func whereIAm() {
+	out, err := exec.Command("pwd").Output()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Printf("your output will be stored in: %s\n", out)
 }
 
-func snap(){
-  now := time.Now()
-  cmd := "imagesnap"
-  fileName := []string{now.Format("20060102150405"), ".jpg"}
-  fileNameFormatted := strings.Join(fileName, "")
-  args := []string{"-w", "1", fileNameFormatted}
-  if err := exec.Command(cmd, args...).Run(); err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
-  }
+func snap() {
+	now := time.Now()
+	cmd := "imagesnap"
+	fileName := []string{now.Format("20060102150405"), ".jpg"}
+	fileNameFormatted := strings.Join(fileName, "")
+	args := []string{"-w", "1", fileNameFormatted}
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
